a25bcb5be4ece1f5cf0e89f1711e61a73b830878
Henning Schei <henning.schei@gmail.com>
2022-12-04 00:17:00 +0100

    des2

---
+++ 2022/des2.py
@@ -0,0 +1,42 @@
+def p1(f):
+    ans = 0
+
+    for line in f:
+        a, b = line.split()
+        a = "ABC".index(a)
+        b = "XYZ".index(b)
+
+        ans += b + 1
+
+        match (b - a) % 3:
+            case 1:
+                ans += 6
+            case 0:
+                ans += 3
+
+    return ans
+def p2(f):
+    ans = 0
+
+    for line in f:
+        a, b = line.split()
+        a = "ABC".index(a)
+
+        match b:
+            case "X":
+                ans += (a - 1) % 3 + 1
+            case "Y":
+                ans += 3
+                ans += a + 1
+            case "Z":
+                ans += 6
+                ans += (a + 1) % 3 + 1
+
+    return ans
+with open('input2.txt', 'r') as f:
+    g = f.readlines()
+
+
+g = [elem.rstrip() for elem in g]
+print (p1(g))
+print (p2(g))
